eab4ac46e706ce541676f41efb24448a648c27ce
Ratu-Dev <queenyash20@gmail.com>
2025-07-24 00:00:58 -0400

    ubah setup link viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ratu-dev.github.io/,
+  base: '/ratu-dev.github.io/',
   plugins: [vue()],
 })
